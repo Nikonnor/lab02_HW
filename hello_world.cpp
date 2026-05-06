0fd83b15216469978a5a0091345c270a2f210d72
Nikonnor <Arakelyannikita.2007@gmail.com>
2026-05-06 10:44:57 +0300

    Изменили code style с помощью clang-format в hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <string>
 
-int main() // Задаем главную функцию.
+int
+main() // Задаем главную функцию.
 {
-	std::string name; // Задаем новую переменную 'name' типа string.
-	std::cout << "Введите имя пользователя: "; std::cin >> name; // Задаем переменную 'name' через стандартный поток ввода.
-	std::cout << "Hello world from " << name << "!" << std::endl; // Выводим результат работы программы.
-	return 0;
+  std::string name; // Задаем новую переменную 'name' типа string.
+  std::cout << "Введите имя пользователя: ";
+  std::cin >> name; // Задаем переменную 'name' через стандартный поток ввода.
+  std::cout << "Hello world from " << name << "!"
+            << std::endl; // Выводим результат работы программы.
+  return 0;
 }
