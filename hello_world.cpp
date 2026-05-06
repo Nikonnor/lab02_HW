344b1f6689f31b0637ab8ac143df61f13c5c23e3
Nikonnor <Arakelyannikita.2007@gmail.com>
2026-05-06 10:26:06 +0300

    Удалили 'using namespace std' из hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main()
 {
-	string name;
-	cout << "Введите имя пользователя: "; cin >> name;
-	cout << "Hello world from " << name << "!" << endl;
+	std::string name;
+	std::cout << "Введите имя пользователя: "; std::cin >> name;
+	std::cout << "Hello world from " << name << "!" << std::endl;
 	return 0;
 }
